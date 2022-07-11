e3b794301f29b1302dd22c4947352652d016eb61
Mirko Basic <bejzik8@gmail.com>
2022-07-11 21:58:53 +0200

    Created enter method in Lottery contract.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,8 +2,15 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
     function Lottery() public {
         manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        players.push(msg.sender);
     }
 }
