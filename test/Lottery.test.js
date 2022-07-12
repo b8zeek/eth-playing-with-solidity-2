e7b2b34138f0021828130fa2743b754e802329cb
Mirko Basic <bejzik8@gmail.com>
2022-07-12 02:05:23 +0200

    Added test for checking whether the contract sends the money to the winner.

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -81,4 +81,19 @@ describe('Lottery contract', () => {
             assert(err);
         }
     });
+
+    it('sends the money to the winner and resets players array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether')
+        });
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+        const difference = finalBalance - initialBalance;
+
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+    })
 })
